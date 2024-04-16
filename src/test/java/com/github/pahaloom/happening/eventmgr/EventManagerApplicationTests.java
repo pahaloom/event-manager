10b309a3e967a702162c55ecbc7d40d2d712bbc9
Martin Ott <pahaloom@gmail.com>
2024-04-16 22:46:43 +0300

    Initial, generated Spring Boot project

---
+++ src/test/java/com/github/pahaloom/happening/eventmgr/EventManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pahaloom.happening.eventmgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
