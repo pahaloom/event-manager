a109bffaf068667b606f3eafe4b9b0bc0cd4b4c1
Martin Ott <pahaloom@gmail.com>
2024-04-18 16:09:36 +0300

    Frontend for event creation

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import * as react from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+
+const Home = react.lazy(() => import("./routes/Home"));
+const Event = react.lazy(() => import("./routes/Event"));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/event">Event</Link></li>
+        </ul>
+
+        <react.Suspense fallback={<>Loading...</>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/event" element={<Event />} />
+          </Routes>
+        </react.Suspense>
+      </div>
+    </Router>
   );
 }
 
---
+++ frontend-react/src/routes/Event.js
@@ -0,0 +1,95 @@
+import { useState } from "react";
+
+const EVENTS_URL = "http://localhost:8080/"
+
+
+
+function App() {
+  const [name, setName] = useState("");
+  const [time, setTime] = useState("");
+  const [place, setPlace] = useState("");
+  const [info, setInfo] = useState("");
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const url = EVENTS_URL + "events";
+    const options = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            "name": name,
+            "time": time,
+            "place": place,
+            "info": info
+        })
+    };
+    fetch(url, options)
+        .then(response => {
+          if (response.ok) {
+            response.json().then(id => {
+              console.log("Created event " + id);
+            });
+          } else {
+            console.log("Error creating event", response);
+          }
+        })
+        .catch(error => console.log("Failed creating event", error));
+  }
+
+  return (
+    <div>
+      <h2>Event</h2>
+      <form onSubmit={handleSubmit}>
+        <table class="form">
+          <tr>
+            <td>
+              <label for="name">Ürituse nimi:</label>
+            </td>
+            <td>
+              <input id="name" name="name" type="text"
+                  value={name || ""}
+                  onChange={e => setName(e.target.value)} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label for="time">Toimumisaeg:</label>
+            </td>
+            <td>
+              <input id="time" name="time" type="text"
+                 value={time || ""}
+                 onChange={e => setTime(e.target.value)} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label for="place">Koht:</label>
+            </td>
+            <td>
+              <input id="place" name="place" type="text"
+                  value={place || ""}
+                  onChange={e => setPlace(e.target.value)} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label for="info">Lisainfo:</label>
+            </td>
+            <td>
+              <textarea id="info" name="info"
+                  value={info || ""}
+                 onChange={e => setInfo(e.target.value)} />
+            </td>
+          </tr>
+          <tr>
+            <td colSpan="2">
+              <button>Tagasi</button> <input type="submit" value="Lisa" />
+            </td>
+          </tr>
+        </table>
+      </form>
+    </div>
+  );
+}
+
+export default App;
