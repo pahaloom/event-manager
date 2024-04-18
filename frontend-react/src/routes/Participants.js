8a6f40941ec2747095f7c15081cbe0ac8aed22fd
Martin Ott <pahaloom@gmail.com>
2024-04-19 01:35:46 +0300

    Conditional form displaying for Physical and Legal person

--- frontend-react/src/routes/Participants.js
+++ frontend-react/src/routes/Participants.js
@@ -10,12 +10,18 @@ function App() {
   const [eventTime, setEventTime] = useState("");
   const [eventPlace, setEventPlace] = useState("");
   const [participants, setParticipants] = useState(null);
-  const [jPFirstName, setJPFirstName] = useState("");
-  const [jPLastName, setJPLastName] = useState("");
+  const [pPFirstName, setPPFirstName] = useState("");
+  const [pPLastName, setPPLastName] = useState("");
+  const [pPCode, setPPCode] = useState("");
+  const [pPMethod, setPPMethod] = useState("");
+  const [pPInfo, setPPInfo] = useState("");
+  const [jPName, setJPName] = useState("");
   const [jPCode, setJPCode] = useState("");
+  const [jPCount, setJPCount] = useState(1);
   const [jPMethod, setJPMethod] = useState("");
   const [jPInfo, setJPInfo] = useState("");
   const [paymentTypes, setPaymentTypes] = useState([]);
+  const [selectedPersonType, setSelectedPersonType] = useState("PHYSICAL");
 
   async function loadMethods() {
     const url = EVENTS_URL + "ptypes";
@@ -104,18 +110,28 @@ function App() {
 
   const handleSubmit = e => {
     e.preventDefault();
+    const data = selectedPersonType === "LEGAL"
+        ? {
+          type: "LEGAL",
+          name: jPName,
+          code: jPCode,
+          count: jPCount,
+          paymentType: jPMethod,
+          info: jPInfo
+        } : {
+          type: "PHYSICAL",
+          firstName: pPFirstName,
+          lastName: pPLastName,
+          code: pPCode,
+          paymentType: pPMethod,
+          info: pPInfo
+        };
+
     const url = EVENTS_URL + "event/" + event.id + "/participants";
     const options = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        type: "PHYSICAL",
-        firstName: jPFirstName,
-        lastName: jPLastName,
-        code: jPCode,
-        paymentType: jPMethod,
-        info: jPInfo
-      })
+      body: JSON.stringify(data)
     };
     fetch(url, options)
         .then(response => {
@@ -123,6 +139,100 @@ function App() {
             loadParticipants(event.id);
           }
         });
+  }
+
+  const handlePersonTypeChange = e => {
+    const type = e.target.value;
+    console.log("Handling person type change", type);
+    setSelectedPersonType(type);
+  }
+
+  function PhysicalForm() {
+    if (selectedPersonType === "PHYSICAL") {
+      return(
+        <form onSubmit={handleSubmit}>
+          <table>
+            <tr>
+              <td><label for="ppfirstname">Eesnimi:</label></td>
+              <td><input id="ppfirstname" name="ppfirstname" type="text"
+                  value={pPFirstName || ""}
+                  onChange={e => setPPFirstName(e.target.value)}/></td>
+            </tr>
+            <tr>
+              <td><label for="pplastname">Perenimi:</label></td>
+              <td><input id="pplastname" name="pplastname" type="text"
+                  value={pPLastName || ""}
+                  onChange={e => setPPLastName(e.target.value)} /></td>
+            </tr>
+            <tr>
+              <td><label for="ppcode">Isikukood:</label></td>
+              <td><input id="ppcode" name="ppcode" type="text"
+                  value={pPCode || ""}
+                  onChange={e => setPPCode(e.target.value)} /></td>
+            </tr>
+            <tr>
+              <td><label for="ppmethod">Maksmisviis:</label></td>
+              <td><select id="ppmethod" name="ppmethod"
+                  defaultValue={pPMethod}
+                  onChange={e => setPPMethod(e.target.value)}>
+                {paymentTypes.map((opt, i) => <option value={opt.code}>{opt.name}</option>)}
+                </select></td>
+            </tr>
+            <tr>
+              <td><label for="ppinfo">Lisainfo:</label></td>
+              <td><textarea id="ppinfo" name="ppinfo"
+                  value={pPInfo || ""}
+                  onChange={e => setPPInfo(e.target.value)} /></td>
+            </tr>
+          </table>
+          <input type="submit" value="Lisa" />
+        </form>
+      );
+    }
+  }
+
+  function JuridicalForm() {
+    if (selectedPersonType === "LEGAL") {
+      return(
+        <form onSubmit={handleSubmit}>
+          <table>
+            <tr>
+              <td><label for="jptname">Juriidiline nimi:</label></td>
+              <td><input id="jpname" name="jpname" type="text"
+                  value={jPName || ""}
+                  onChange={e => setJPName(e.target.value)}/></td>
+            </tr>
+            <tr>
+              <td><label for="jpcode">Registrikood:</label></td>
+              <td><input id="jpcode" name="jpcode" type="text"
+                  value={jPCode || ""}
+                  onChange={e => setJPCode(e.target.value)} /></td>
+            </tr>
+            <tr>
+              <td><label for="jpcount">Osavõtjate arv:</label></td>
+              <td><input id="jpcount" name="jpcount" type="number"
+                  value={jPCount || ""}
+                  onChange={e => setJPCount(e.target.value)} /></td>
+            </tr>
+            <tr>
+              <td><label for="jpmethod">Maksmisviis:</label></td>
+              <td><select id="jpmethod" name="jpmethod"
+                  defaultValue={jPMethod}
+                  onChange={e => setJPMethod(e.target.value)}>
+                {paymentTypes.map((opt, i) => <option value={opt.code}>{opt.name}</option>)}
+                </select></td>
+            </tr>
+            <tr>
+              <td><label for="jpinfo">Lisainfo:</label></td>
+              <td><textarea id="jpinfo" name="jpinfo"
+                  value={jPInfo || ""}
+                  onChange={e => setJPInfo(e.target.value)} /></td>
+            </tr>
+          </table>
+          <input type="submit" value="Lisa" />
+        </form>
+      );
+    }
   }
 
   return(
@@ -149,43 +259,14 @@ function App() {
         </td>
       </table>
       <h2>Osavõtjate lisamine</h2>
-      <form onSubmit={handleSubmit}>
-        <table>
-          <tr>
-            <td><label for="jpfirstname">Eesnimi:</label></td>
-            <td><input id="jpfirstname" name="jpfirstname" type="text"
-                value={jPFirstName || ""}
-                onChange={e => setJPFirstName(e.target.value)}/></td>
-          </tr>
-          <tr>
-            <td><label for="jplastname">Perenimi:</label></td>
-            <td><input id="jplastname" name="jplastname" type="text"
-                value={jPLastName || ""}
-                onChange={e => setJPLastName(e.target.value)} /></td>
-          </tr>
-          <tr>
-            <td><label for="jpcode">Isikukood:</label></td>
-            <td><input id="jpcode" name="jpcode" type="text"
-                value={jPCode || ""}
-                onChange={e => setJPCode(e.target.value)} /></td>
-          </tr>
-          <tr>
-            <td><label for="jpmethod">Maksmisviis:</label></td>
-            <td><select id="jpmethod" name="jpmethod"
-                defaultValue={jPMethod}
-                onChange={e => setJPMethod(e.target.value)}>
-              {paymentTypes.map((opt, i) => <option value={opt.code}>{opt.name}</option>)}
-              </select></td>
-          </tr>
-          <tr>
-            <td><label for="jpinfo">Lisainfo:</label></td>
-            <td><textarea id="jpinfo" name="jpinfo"
-                value={jPInfo || ""}
-                onChange={e => setJPInfo(e.target.value)} /></td>
-          </tr>
-        </table>
-        <input type="submit" value="Lisa" />
-      </form>
+      <label><input type="radio" name="personType" value="PHYSICAL"
+          checked={selectedPersonType === "PHYSICAL"}
+          onChange={handlePersonTypeChange}/>Eraisik</label>
+      <label><input type="radio" name="personType" value="LEGAL"
+          checked={selectedPersonType === "LEGAL"}
+          onChange={handlePersonTypeChange} />Ettevõte</label>
+      <PhysicalForm />
+      <JuridicalForm />
     </div>
   );
 }
